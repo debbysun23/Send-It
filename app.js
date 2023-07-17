670648714d408fd237296b06a7ea36f4c5177873
debbysun23 <deborahokafor7@gmail.com>
2023-07-17 18:22:43 +0100

    initial

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/api/v1/user', (req, res) =>{
     res.status(200).send('welcome page')
 })
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
