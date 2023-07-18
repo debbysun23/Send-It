38cba711a22906389fc65b64e46e145876798548
debbysun23 <deborahokafor7@gmail.com>
2023-07-18 20:17:39 +0100

    initial

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(express.json())
 
 app.use(helmet())
 app.use(cors({
-    origin: "http://127.0.0.1:5500"
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200
 }))
 app.use(xss())
 app.use(bodyParser.json())
@@ -88,7 +90,7 @@ app.get('/api/v1/user', (req, res) =>{
     res.status(200).send('welcome page')
 })
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', ' Content-Type');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
